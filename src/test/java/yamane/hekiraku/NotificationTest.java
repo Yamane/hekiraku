f2758a73815bfa86baad524a64b535a3c3281253
Yamane <yamane.eee@gmail.com>
2024-05-13 17:42:38 +0900

    不要なコメントを削除。コンソール出力内容を変更。

--- src/test/java/yamane/hekiraku/NotificationTest.java
+++ src/test/java/yamane/hekiraku/NotificationTest.java
@@ -14,7 +14,7 @@ public class NotificationTest extends AbstractTest {
   @Test
   public void listNotificationsTest() throws Exception {
     PageableList<Notification> notifications = session.notification().list(10, null);
-    notifications.forEach(n -> System.out.println(toString(n)));
+    notifications.forEach(n -> System.out.println(n.jsonString()));
   }
   
   @Test
@@ -27,12 +27,5 @@ public class NotificationTest extends AbstractTest {
     Integer count = session.notification().getUnreadCount(null);
     assertNotNull(count);
   }
-  
-  private String toString(Notification n) {
-    StringBuilder b = new StringBuilder();
-    b.append(n.getAuthor().getDisplayName());
-    b.append("\n");
-    b.append(n.getReason());
-    return b.toString();
-  }
+
 }
